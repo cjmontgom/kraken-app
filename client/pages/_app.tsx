00aa6eb8f1896fa5e5d431aecd7c8deddca21e36
chloe <chloemontgomery01@gmail.com>
2024-05-10 15:17:37 +0100

    Migrate from pure CSS to styled-components

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,39 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: ${(props) => props.theme.fontFamily};
+      color: ${(props) => props.theme.colours.ice};
+    }
+
+    body {
+      background: ${(props) => props.theme.colours.siphon};
+    }
+`;
+
+const theme = {
+  colours: {
+    hemocyanin: "#180048",
+    ice: "#f0ffff",
+    plum: "#600e6b",
+    purpleHaze: "#a49fc8",
+    siphon: "#100030",
+    sohoLights: "#f050f8",
+  },
+  fontFamily: "Gotham, helvetica, arial, sans-serif",
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
